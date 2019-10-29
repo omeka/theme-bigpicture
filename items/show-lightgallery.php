78a2fb7933f56e6186299ea8850ded68610db5e6
Kim Nguyen <design@kimisgold.com>
2019-10-29 12:36:30 -0400

    Remove chocolat option, provide file link in viewer.

--- items/show-lightgallery.php
+++
@@ -1,9 +0,0 @@
-<ul id="itemfiles" <?php echo (count($images) == 1) ? 'class="solo"' : ''; ?>>
-    <?php foreach ($images as $image): ?>
-    <li data-src="<?php echo $image->getWebPath('original'); ?>" data-thumb="<?php echo $image->getWebPath('square_thumbnail'); ?>" class="media resource">
-        <div class="media-render">
-        <?php echo file_image('original', array(), $image); ?>
-        </div>
-    </li>
-    <?php endforeach; ?>
-</ul>
--- js/items-show.js
+++ js/items-show.js
@@ -3,28 +3,6 @@ if (!BigPicture) {
 }
 
 (function($) {
-    BigPicture.useChocolat = function() {
-        var inContainer = $("#itemfiles-nav").Chocolat({
-        imageSize: "default",
-        loop: true,
-        container: "#itemfiles-stage",
-        }).data("chocolat");
-    
-        inContainer.api().open()
-        inContainer.api().getElem("overlay").off("click.chocolat");
-    
-        $(document).off("keydown.chocolat").on("keydown.chocolat", function(e) {
-            if (inContainer.api().get("initialized")) {
-                if (e.keyCode == 37) {
-                    inContainer.api().prev()
-                }
-                else if (e.keyCode == 39) {
-                    inContainer.api().next()
-                }
-            }
-        });
-    };
-    
     BigPicture.useLightslider = function(imageCount) {
         if ($.isFunction($.fn.lightSlider)) {
             $('#itemfiles').lightSlider({
