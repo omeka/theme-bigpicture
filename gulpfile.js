4b36ad11c6458e233b8ff4173c7508fee0c569f7
Kim Nguyen <design@kimisgold.com>
2022-02-04 15:50:45 -0500

    Bump versions, update dependencies, recompile css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp');
 
 gulp.task('css', function() {
-    var sass = require('gulp-sass');
+    var sass = require('gulp-sass')(require('sass'));
     var postcss = require('gulp-postcss');
     var autoprefixer = require('autoprefixer');
 
     return gulp.src('./css/sass/*.scss')
         .pipe(sass({
-            outputStyle: 'nested',
             includePaths: ['./node_modules/susy/sass']
         }).on('error', sass.logError))
         .pipe(postcss([autoprefixer]))
