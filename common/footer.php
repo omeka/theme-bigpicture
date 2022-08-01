6c304d01572713ffa98fad45f4b73221eb6a13ac
Kim Nguyen <design@kimisgold.com>
2022-08-01 17:05:31 -0400

    Remove unnecessary role.

--- common/footer.php
+++ common/footer.php
@@ -1,7 +1,6 @@
         </article>
 
-        <footer role="contentinfo">
-
+        <footer>
             <div id="footer-text">
                 <?php echo get_theme_option('Footer Text'); ?>
                 <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
