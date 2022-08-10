7d45b9becb9df33de808b66165905224d7ffd4f5
Kim Nguyen <design@kimisgold.com>
2022-08-10 15:17:46 -0400

    Check for files on collection/show for thumbnail.

--- collections/show.php
+++ collections/show.php
@@ -19,7 +19,7 @@ $perPagePublic = get_option('per_page_public')
             <?php foreach (loop('items') as $item): ?>
             <?php $itemTitle = metadata('item', 'display_title'); ?>
             <div class="item hentry">
-                <?php if (metadata('item', 'has thumbnail')): ?>
+                <?php if (metadata('item', 'has files')): ?>
                 <div class="item-img">
                     <?php $imageSize = (get_option('use_square_thumbnail') == 1) ? 'square_thumbnail' : 'fullsize'; ?>
                     <?php echo link_to_item(item_image($imageSize, array('alt' => $itemTitle))); ?>
