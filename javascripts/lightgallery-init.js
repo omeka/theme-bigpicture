92eb3976f4ca93fc09f56e1f261c184ad5936bda
Kim Nguyen <design@kimisgold.com>
2025-08-12 12:43:49 -0400

    Update caption setting to match core defaults.
    See https://github.com/omeka/Omeka/commit/c422ecea75bf5a545727113ece8f5b7ee1ea4e0d#diff-2193fff8599606bc288d3c2d855d03d04c58a858fedd6c19b36cae3ec9e05c41R16.

--- javascripts/lightgallery-init.js
+++ javascripts/lightgallery-init.js
@@ -19,6 +19,7 @@
             showZoomInOutIcons: true,
             captions: true,
             allowMediaOverlap: false,
+            getCaptionFromTitleOrAlt: false,
             scale: zoomScale
         });
 
